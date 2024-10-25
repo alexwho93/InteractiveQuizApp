ebf5bc9f4d3b4d3ddd1066f06f3364473fb1d44e
alexwho93 <25087931+alexwho93@users.noreply.github.com>
2024-10-25 14:35:45 +0000

    fixing acessing file on vercel

--- app/api/questions/[categoryId]/route.js
+++ app/api/questions/[categoryId]/route.js
@@ -2,7 +2,10 @@ import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
-  const file = await fs.readFile(process.cwd() + "/app/questions.json", "utf8");
+  const file = await fs.readFile(
+    process.cwd() + "/public/questions.json",
+    "utf8"
+  );
   const data = JSON.parse(file);
   return NextResponse.json({ data: data[params.categoryId] });
 }
