d2aaa750851bb9cc869f5cd2aabcf561cc90cd57
alexwho93 <25087931+alexwho93@users.noreply.github.com>
2024-10-25 15:54:59 +0000

    another fix for vercel

--- app/api/questions/[categoryId]/route.js
+++ app/api/questions/[categoryId]/route.js
@@ -1,11 +1,11 @@
 import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
+import path from "path";
 
 export async function GET(request, { params }) {
-  const file = await fs.readFile(
-    process.cwd() + "/public/questions.json",
-    "utf8"
-  );
+  const filePath = path.join(process.cwd(), "public", "questions.json");
+  const file = await fs.readFile(filePath, "utf8");
   const data = JSON.parse(file);
+
   return NextResponse.json({ data: data[params.categoryId] });
 }
